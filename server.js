70f28541744ab9616c31134ead4ec75bd22adebe
Holly MacLean <mariettia@gmail.com>
2025-05-16 22:55:58 -0400

    Fixing upload errors

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use('/api/posts', postRoutes);
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static('uploads', {
+  setHeaders: (res, path) => {
+    res.set('Cross-Origin-Resource-Policy', 'cross-origin');
+  }
+}));
 app.use('/api/auth', authRoutes); // use auth routes
 
 const mongoose = require('mongoose');
